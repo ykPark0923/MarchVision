bb26bc1ba9937928a0960adb321dc4bb6431ac49
AidenLee76 <masklee76@gmail.com>
2025-03-15 20:28:53 +0900

    모델트리 추가

--- JidamVision/MainForm.cs
+++ JidamVision/MainForm.cs
@@ -48,6 +48,10 @@ namespace JidamVision
             var resultWindow = new ResultForm();
             resultWindow.Show(cameraWindow.Pane, DockAlignment.Bottom, 0.3);
 
+            //검사 결과창 우측에 40% 비율로 모델트리 추가
+            var modelTreeWindow = new ModelTreeForm();
+            modelTreeWindow.Show(resultWindow.Pane, DockAlignment.Right, 0.4);
+
             //속성창 추가
             var propWindow = new PropertiesForm();
             propWindow.Show(_dockPanel, DockState.DockRight);
---
+++ JidamVision/ModelTreeForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace JidamVision
+{
+    public partial class ModelTreeForm : DockContent
+    {
+        private ContextMenuStrip _contextMenu;
+
+        public ModelTreeForm()
+        {
+            InitializeComponent();
+
+            tvModelTree.Nodes.Add("Root");
+
+            // 컨텍스트 메뉴 초기화
+            _contextMenu = new ContextMenuStrip();
+            ToolStripMenuItem addBaseRoiItem = new ToolStripMenuItem("Base ROI", null, AddNode_Click) { Tag = "Base" };
+            ToolStripMenuItem addSubRoiItem = new ToolStripMenuItem("Sub ROI", null, AddNode_Click) { Tag = "Sub" };
+            ToolStripMenuItem addIdRoiItem = new ToolStripMenuItem("ID ROI", null, AddNode_Click) { Tag = "ID" };
+
+            _contextMenu.Items.Add(addBaseRoiItem);
+            _contextMenu.Items.Add(addSubRoiItem);
+            _contextMenu.Items.Add(addIdRoiItem);
+        }
+
+        private void tvModelTree_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                TreeNode clickedNode = tvModelTree.GetNodeAt(e.X, e.Y);
+                if (clickedNode != null && clickedNode.Text == "Root") ;
+                {
+                    tvModelTree.SelectedNode = clickedNode;
+                    _contextMenu.Show(tvModelTree, e.Location);
+                }
+            }
+        }
+
+        private void AddNode_Click(object sender, EventArgs e)
+        {
+            if (tvModelTree.SelectedNode != null & sender is ToolStripMenuItem)
+            {
+                ToolStripMenuItem menuItem = (ToolStripMenuItem)sender;
+                string nodeType = menuItem.Tag?.ToString();
+                if (nodeType == "Base")
+                {
+                    tvModelTree.SelectedNode.Nodes.Add("Base01");
+                }
+                else if (nodeType == "Sub")
+                {
+                    tvModelTree.SelectedNode.Nodes.Add("Sub");
+                }
+                else if (nodeType == "ID")
+                {
+                    tvModelTree.SelectedNode.Nodes.Add("ID");
+                }
+            }
+        }
+    }
+}
